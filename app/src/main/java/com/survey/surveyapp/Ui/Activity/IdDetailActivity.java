8e8e2a52dac2de464265c316761737e2ebaa9b16
rajeev1995rajan@gmail.com <Rajkumu@6>
2021-02-09 19:10:51 +0530

    Second commit

--- app/src/main/java/com/survey/surveyapp/Ui/Activity/IdDetailActivity.java
+++ app/src/main/java/com/survey/surveyapp/Ui/Activity/IdDetailActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.survey.surveyapp.R;
 
 public class IdDetailActivity extends AppCompatActivity {
 
-    ImageView imageView_back;
+    ImageView imageView_back,imag4e_edit,image_ok;
+    TextView tv_cont,tv_add,tv_desi,tv_email,tv_gender,tv_name;
+    EditText et_cont,et_add,et_desi,et_email,et_gender,et_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +22,84 @@ public class IdDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_id_detail);
 
         imageView_back=findViewById(R.id.back1_rep_iddetail);
+        imag4e_edit=findViewById(R.id.image_edit_id);
+        image_ok=findViewById(R.id.image_ok_id);
 
+        tv_name=findViewById(R.id.tv_name_id);
+        tv_cont=findViewById(R.id.tv_cont_id);
+        tv_add=findViewById(R.id.tv_add_id);
+        tv_desi=findViewById(R.id.tv_desig_id);
+        tv_email=findViewById(R.id.tv_email_id);
+        tv_gender=findViewById(R.id.tv_gender_id);
+
+        et_name=findViewById(R.id.et_name_id);
+        et_cont=findViewById(R.id.et_cont_id);
+        et_add=findViewById(R.id.et_add_id);
+        et_desi=findViewById(R.id.et_desig_id);
+        et_email=findViewById(R.id.et_email_id);
+        et_gender=findViewById(R.id.et_gender_id);
+
+
+
+
+        imag4e_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imag4e_edit.setVisibility(View.GONE);
+                image_ok.setVisibility(View.VISIBLE);
+
+
+
+
+                tv_name.setVisibility(View.GONE);
+                tv_cont.setVisibility(View.GONE);
+                tv_add.setVisibility(View.GONE);
+                tv_desi.setVisibility(View.GONE);
+                tv_email.setVisibility(View.GONE);
+                tv_gender.setVisibility(View.GONE);
+
+
+                et_name.setVisibility(View.VISIBLE);
+                et_cont.setVisibility(View.VISIBLE);
+                et_add.setVisibility(View.VISIBLE);
+                et_desi.setVisibility(View.VISIBLE);
+                et_email.setVisibility(View.VISIBLE);
+                et_gender.setVisibility(View.VISIBLE);
+
+
+                et_name.setText(tv_name.getText().toString().trim());
+                et_cont.setText(tv_cont.getText().toString().trim());
+                et_add.setText(tv_add.getText().toString().trim());
+                et_desi.setText(tv_desi.getText().toString().trim());
+                et_email.setText(tv_email.getText().toString().trim());
+                et_gender.setText(tv_gender.getText().toString().trim());
+            }
+        });
+
+        image_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imag4e_edit.setVisibility(View.VISIBLE);
+                image_ok.setVisibility(View.GONE);
+
+                tv_cont.setVisibility(View.VISIBLE);
+                tv_add.setVisibility(View.VISIBLE);
+                tv_desi.setVisibility(View.VISIBLE);
+                tv_email.setVisibility(View.VISIBLE);
+                tv_gender.setVisibility(View.VISIBLE);
+                tv_name.setVisibility(View.VISIBLE);
+
+
+                et_cont.setVisibility(View.GONE);
+                et_add.setVisibility(View.GONE);
+                et_desi.setVisibility(View.GONE);
+                et_email.setVisibility(View.GONE);
+                et_gender.setVisibility(View.GONE);
+                et_name.setVisibility(View.GONE);
+            }
+        });
 
         imageView_back.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/survey/surveyapp/Ui/Activity/ProfileActivity.java
+++ app/src/main/java/com/survey/surveyapp/Ui/Activity/ProfileActivity.java
@@ -3,15 +3,134 @@ package com.survey.surveyapp.Ui.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.survey.surveyapp.R;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView imageView_edit, imageView_ok, image_edit_detail, image_ok_detail,image_back;
+    TextView textView_name, tv_contact, tv_city, tv_designation, tv_email, tv_typehere, tv_alt_contact;
+    EditText et_name, et_contact, et_city, et_designation, et_email, et_typehere, et_alt_contact;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_profile);
+
+        imageView_edit = findViewById(R.id.edtit_pen_name);
+        image_edit_detail = findViewById(R.id.image_editdetal_pen);
+        image_back = findViewById(R.id.back1_rep_profile);
+        imageView_ok = findViewById(R.id.image_name_ok_button);
+        image_ok_detail = findViewById(R.id.ok_button_detail);
+
+
+        textView_name = findViewById(R.id.tv_name);
+        et_name = findViewById(R.id.et_name);
+
+
+        //Detailkinfo views
+        tv_contact = findViewById(R.id.tv_cont_detail);
+        tv_city = findViewById(R.id.tv_city_detail);
+        tv_designation = findViewById(R.id.tv_designation_detail);
+        tv_email = findViewById(R.id.tv_email_detail);
+        tv_typehere = findViewById(R.id.tv_typehere_detail);
+        tv_alt_contact = findViewById(R.id.tv_alternatecontact_detail);
+        ;
+
+        et_contact = findViewById(R.id.et_cont_detail);
+        et_city = findViewById(R.id.et_city_detail);
+        et_designation = findViewById(R.id.et_designation_detail);
+        et_email = findViewById(R.id.et_email_detail);
+        et_typehere = findViewById(R.id.et_typehere_detail);
+        et_alt_contact = findViewById(R.id.et_alternatecontact_detail);
+
+        imageView_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_name.setText(textView_name.getText().toString().trim());
+                et_name.setVisibility(View.VISIBLE);
+                textView_name.setVisibility(View.GONE);
+                imageView_edit.setVisibility(View.GONE);
+                imageView_ok.setVisibility(View.VISIBLE);
+            }
+        });
+
+        imageView_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_name.setVisibility(View.GONE);
+                textView_name.setVisibility(View.VISIBLE);
+                imageView_edit.setVisibility(View.VISIBLE);
+                imageView_ok.setVisibility(View.GONE);
+            }
+        });
+
+        image_edit_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                image_ok_detail.setVisibility(View.VISIBLE);
+                image_edit_detail.setVisibility(View.GONE);
+
+                et_contact.setText(tv_contact.getText().toString().trim());
+                et_city.setText(tv_city.getText().toString().trim());
+                et_designation.setText(tv_designation.getText().toString().trim());
+                et_email.setText(tv_email.getText().toString().trim());
+                et_typehere.setText(tv_typehere.getText().toString().trim());
+                et_alt_contact.setText(tv_alt_contact.getText().toString().trim());
+
+
+                et_contact.setVisibility(View.VISIBLE);
+                et_city.setVisibility(View.VISIBLE);
+                et_designation.setVisibility(View.VISIBLE);
+                et_email.setVisibility(View.VISIBLE);
+                et_typehere.setVisibility(View.VISIBLE);
+                et_alt_contact.setVisibility(View.VISIBLE);
+
+
+                tv_contact.setVisibility(View.GONE);
+                tv_city.setVisibility(View.GONE);
+                tv_designation.setVisibility(View.GONE);
+                tv_email.setVisibility(View.GONE);
+                tv_typehere.setVisibility(View.GONE);
+                tv_alt_contact.setVisibility(View.GONE);
+
+            }
+        });
+        image_ok_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                image_ok_detail.setVisibility(View.GONE);
+                image_edit_detail.setVisibility(View.VISIBLE);
+                et_contact.setVisibility(View.GONE);
+                et_city.setVisibility(View.GONE);
+                et_designation.setVisibility(View.GONE);
+                et_email.setVisibility(View.GONE);
+                et_typehere.setVisibility(View.GONE);
+                et_alt_contact.setVisibility(View.GONE);
+
+
+                tv_contact.setVisibility(View.VISIBLE);
+                tv_city.setVisibility(View.VISIBLE);
+                tv_designation.setVisibility(View.VISIBLE);
+                tv_email.setVisibility(View.VISIBLE);
+                tv_typehere.setVisibility(View.VISIBLE);
+                tv_alt_contact.setVisibility(View.VISIBLE);
+            }
+        });
+
+        image_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
 }
