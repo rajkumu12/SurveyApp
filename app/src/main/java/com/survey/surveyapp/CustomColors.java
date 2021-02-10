7ba260fff1986aabce0bd393931bf3e51f3c4f42
rajeev1995rajan@gmail.com <Rajkumu@6>
2021-02-10 18:23:44 +0530

    tab highlights

---
+++ app/src/main/java/com/survey/surveyapp/CustomColors.java
@@ -0,0 +1,10 @@
+package com.survey.surveyapp;
+
+import android.graphics.Color;
+
+public class CustomColors {
+
+    public static int white= Color.rgb(255, 255, 255);
+    public static int black= Color.rgb(0, 0, 0);
+    public static int light_blue= Color.rgb(140, 244, 255);
+}
--- app/src/main/java/com/survey/surveyapp/Ui/Activity/HomeActivity.java
+++ app/src/main/java/com/survey/surveyapp/Ui/Activity/HomeActivity.java
@@ -6,12 +6,14 @@ import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.survey.surveyapp.ColorTransparentUtils;
 import com.survey.surveyapp.Ui.Fragments.HomeFragments;
 import com.survey.surveyapp.R;
 import com.survey.surveyapp.FullDrawerLayout;
@@ -19,7 +21,7 @@ import com.survey.surveyapp.FullDrawerLayout;
 public class HomeActivity extends AppCompatActivity  implements View.OnClickListener{
     FullDrawerLayout drawerLayout;
     ImageView imageView_hamburgericon,close_drawer,img_profile;
-    TextView tv_daily_report_nav,tv_notifications_nav;
+    TextView tv_home,tv_daily_report_nav,tv_survey,tv_notifications_nav,tv_setting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +31,11 @@ public class HomeActivity extends AppCompatActivity  implements View.OnClickList
         drawerLayout = findViewById(R.id.drawer);
         imageView_hamburgericon = findViewById(R.id.hamburger);
         close_drawer =findViewById(R.id.close_drawer);
+        tv_home=findViewById(R.id.tv_home_nav);
         tv_daily_report_nav =findViewById(R.id.tv_daily_rep_nav);
-        tv_notifications_nav =findViewById(R.id.tv_notifications);
+        tv_survey =findViewById(R.id.tv_survey_nav);
+        tv_notifications_nav =findViewById(R.id.tv_notifications_nav);
+        tv_setting =findViewById(R.id.tv_setting_nav);
         img_profile =findViewById(R.id.img_profile);
 
 
@@ -45,11 +50,30 @@ public class HomeActivity extends AppCompatActivity  implements View.OnClickList
 
 
         close_drawer.setOnClickListener(this);
+        tv_home.setOnClickListener(this);
         tv_daily_report_nav.setOnClickListener(this);
+        tv_survey.setOnClickListener(this);
         tv_notifications_nav.setOnClickListener(this);
+        tv_setting.setOnClickListener(this);
         img_profile.setOnClickListener(this);
 
         loadFragment(new HomeFragments());
+
+        activeTab(tv_home,tv_daily_report_nav,tv_survey,tv_notifications_nav,tv_setting);
+
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void activeTab(TextView active, TextView tv_daily_report_nav,
+                           TextView tv_survey, TextView tv_notifications_nav, TextView tv_setting) {
+
+        active.setTextColor(Color.WHITE);
+        tv_daily_report_nav.setTextColor(Color.parseColor("#80FFFFFF"));
+        tv_survey.setTextColor(Color.parseColor("#80FFFFFF"));
+        tv_notifications_nav.setTextColor(Color.parseColor("#80FFFFFF"));
+        tv_setting.setTextColor(Color.parseColor("#80FFFFFF"));
+
+
     }
 
     public boolean loadFragment(Fragment fragment) {
@@ -71,10 +95,20 @@ public class HomeActivity extends AppCompatActivity  implements View.OnClickList
         if (id==R.id.close_drawer){
             if(!drawerLayout.isDrawerOpen(GravityCompat.START)) drawerLayout.openDrawer(Gravity.START);
             else drawerLayout.closeDrawer(Gravity.START);
+        }else if (id==R.id.tv_home_nav){
+            loadFragment(new HomeFragments());
+            activeTab(tv_home,tv_daily_report_nav,tv_survey,tv_notifications_nav,tv_setting);
         }else if (id==R.id.tv_daily_rep_nav){
             startActivity(new Intent(HomeActivity.this,ReportActivity.class));
-        }else if (id==R.id.tv_notifications){
+            activeTab(tv_daily_report_nav,tv_home,tv_survey,tv_notifications_nav,tv_setting);
+        }else if (id==R.id.tv_survey_nav){
+            activeTab(tv_survey,tv_daily_report_nav,tv_home,tv_notifications_nav,tv_setting);
+        } else if (id==R.id.tv_notifications_nav){
             startActivity(new Intent(HomeActivity.this,Notifications.class));
+            activeTab(tv_notifications_nav,tv_survey,tv_daily_report_nav,tv_home,tv_setting);
+        }else if (id==R.id.tv_setting_nav){
+            startActivity(new Intent(HomeActivity.this,Notifications.class));
+            activeTab(tv_setting,tv_notifications_nav,tv_survey,tv_daily_report_nav,tv_home);
         }else if (id==R.id.img_profile){
             startActivity(new Intent(HomeActivity.this,ProfileActivity.class));
         }
